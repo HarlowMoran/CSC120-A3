97c362b6870b8e7dd79c7cbb29cdfcf51182249b
HarlowMoran <hmoran72@smith.edu>
2025-02-19 19:24:00 -0500

    Initial Commit in Converstation.java

--- Conversation.java
+++ Conversation.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 class Conversation implements Chatbot {
 
   // Attributes 
@@ -7,14 +7,45 @@ class Conversation implements Chatbot {
    * Constructor 
    */
   Conversation() {
+    /**Ask user to imput the number of rounds */
+    Scanner imput = new Scanner(System.in);
+    System.out.println("How many rounds?");
+    int rounds = imput.nextInt();
+
+    /**Engage in back-and-forth with the user */
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Welcome! What's up?");
+
+    /**Create a while loop where the user is given a response and prompted to say more for 1 less rounds than however many rounds they entered*/
+    int i = 0;
+    while (i < (rounds-1)){
+      i++;
+      String response = scanner.nextLine();
+      if(response.equals("")){
+        System.out.println("What's up?");
+      } 
+      else{
+        System.out.println("Intersting. Tell me more!");
+      }
+      
+    }
     
+    /*give the final round some variation, and inform the user that the chat is coming to an end */
+    String response = scanner.nextLine();
+    System.out.println("Is there anything else you would like to share with me before we finish chatting?");
+    String finalResponse = scanner.nextLine();
+    
+    /**Ends the conversation */
+    System.out.println("Okay! I have enjoyed our converstion. Thanks for chatting!");
+
+
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-
+    
   }
 
   /**
--- reflection.md
+++ reflection.md
@@ -5,3 +5,10 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+1. I consulted with Eva and Milika for this assignment. 
+2. 
+3. I am in an unusual situation where I have only ever coded in Java. So while on the last assignment I felt like I was scrabling to understnd syntax, this  assignment felt a lot easier because all I had to worry about was fufilling the actual assignment, rather than figuring out the language simultaneously with the assignment.
+4. It seems like the general structure in the same between the two langauges. For example, classes and methods seem fairly similar.
+5. I was very thankful to have remebered the use of Scanner. I didn't really understand how to alter the responses, though, looking for keywords.
